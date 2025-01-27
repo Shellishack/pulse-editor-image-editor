dbc2212fe18eac01e8e6586db9f973b1513057ba
Ellis Shang <40737228+Shellishack@users.noreply.github.com>
2025-01-28 06:40:36 +0800

    Add pulse.config.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 import { resolve, dirname } from "path";
 import { fileURLToPath } from "url";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import CopyPlugin from "copy-webpack-plugin";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -14,9 +15,20 @@ const config = {
   resolve: {
     extensions: [".ts", ".tsx", ".js"],
   },
-  plugins: [new MiniCssExtractPlugin({
-    filename: "globals.css"
-  })],
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "globals.css",
+    }),
+    // Copy ./pulse.config.json to ./dist/pulse.config.json
+    new CopyPlugin({
+      patterns: [
+        {
+          from: resolve(__dirname, "pulse.config.json"),
+          to: resolve(__dirname, "dist"),
+        },
+      ],
+    }),
+  ],
   module: {
     rules: [
       { test: /\.tsx?$/, use: "ts-loader" },
