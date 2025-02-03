125e7c9219b3ef5e8bcdf06c8d7ca498791b3d5d
Ellis Shang <40737228+Shellishack@users.noreply.github.com>
2025-02-03 14:38:28 +0800

    Allow extension as module federation producer (remote)

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./tailwind.css";
+import { ExtensionConfig, ExtensionTypeEnum } from "@pulse-editor/types";
+
+/**
+ * Pulse Editor Extension Config
+ *
+ */
+export const Config: ExtensionConfig = {
+  // Do not use hyphen character '-' in the name
+  name: "pulse_extension_template",
+  description: "Pulse extension template",
+  version: "v0.0.1",
+  extensionType: ExtensionTypeEnum.FileView,
+  fileTypes: ["txt", "json", "py", "cpp", "c", "tsx", "ts", "js", "jsx"],
+};
 
 export default function Main() {
-  return <div className="text-blue-400 pt-20">plugin..</div>;
+  const [count, setCount] = useState<number>(0);
+
+  return (
+    <div>
+      <button
+        className="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+        onClick={() => setCount(count + 1)}
+      >
+        Click me
+      </button>
+      <p className="text-blue-400">{count}</p>
+    </div>
+  );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,29 @@
-import { resolve, dirname } from "path";
+import { dirname, join } from "path";
 import { fileURLToPath } from "url";
+import { ModuleFederationPlugin } from "@module-federation/enhanced/webpack";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import CopyPlugin from "copy-webpack-plugin";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const modulePath = "http://localhost:3001/pulse_extension_template/v0.0.1/";
 
 const config = {
   entry: "./src/main.tsx",
   output: {
-    filename: "bundle.js",
-    path: resolve(__dirname, "dist"),
-    libraryTarget: "module",
+    publicPath: modulePath,
+    // publicPath: "auto",
+  },
+  devServer: {
+    static: {
+      directory: join(__dirname, "dist"),
+    },
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "X-Requested-With, content-type, Authorization",
+    },
+    port: 3001,
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js"],
@@ -19,14 +32,35 @@ const config = {
     new MiniCssExtractPlugin({
       filename: "globals.css",
     }),
-    // Copy ./pulse.config.json to ./dist/pulse.config.json
-    new CopyPlugin({
-      patterns: [
-        {
-          from: resolve(__dirname, "pulse.config.json"),
-          to: resolve(__dirname, "dist"),
+    new ModuleFederationPlugin({
+      // Do not use hyphen character '-' in the name
+      name: "pulse_extension_template",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./main": "./src/main",
+      },
+      shared: {
+        react: {
+          requiredVersion: "19.0.0-rc-65e06cb7-20241218",
+          import: "react", // the "react" package will be used a provided and fallback module
+          shareKey: "react", // under this name the shared module will be placed in the share scope
+          shareScope: "default", // share scope with this name will be used
+          singleton: true, // only a single version of the shared module is allowed
         },
-      ],
+        "react-dom": {
+          requiredVersion: "19.0.0-rc-65e06cb7-20241218",
+          singleton: true, // only a single version of the shared module is allowed
+        },
+        // Share Workbox configuration as a module
+        "workbox-webpack-plugin": {
+          shareConfig: {
+            singleton: true,
+            requiredVersion: "^7.3.0",
+          },
+        },
+      },
+
+      getPublicPath: `function() {return "${modulePath}"}`,
     }),
   ],
   module: {
@@ -55,9 +89,6 @@ const config = {
         ],
       },
     ],
-  },
-  experiments: {
-    outputModule: true, // This enables ES module output
   },
 };
 
