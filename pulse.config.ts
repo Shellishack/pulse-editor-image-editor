f1f29211576bf86c7525342e3845f72fe7609d1a
Shellishack <40737228+Shellishack@users.noreply.github.com>
2025-05-24 22:01:56 +0800

    Add image generation extension and expose commands

--- pulse.config.ts
+++ pulse.config.ts
@@ -1,5 +1,6 @@
 import { ExtensionConfig, ExtensionTypeEnum } from "@pulse-editor/shared-utils";
 import packageJson from "./package.json" with { type: "json" };
+import { generateImageCommandInfo } from "./src/lib/commands";
 
 /**
  * Pulse Editor Extension Config
@@ -14,6 +15,9 @@ const config: ExtensionConfig = {
   version: packageJson.version,
   extensionType: ExtensionTypeEnum.FileView,
   fileTypes: ["peie.image"],
+  commandsInfoList: [
+    generateImageCommandInfo
+  ]
 };
 
 export default config;
